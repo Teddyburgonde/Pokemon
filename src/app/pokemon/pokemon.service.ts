6ed091ba3c582521a750630733365d9f41fc0b3c
Teddy Bandama <teddybandama@MacBook-Pro-de-Teddy.local>
2022-12-11 09:35:47 +0100

    Creation de 3 methodes dans pokemons service, getPokemonList, getPokemonById, getPokemonTypeList

---
+++ src/app/pokemon/pokemon.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+
+  getPokemonList(): Pokemon[] {
+    return POKEMONS;
+  }
+
+  getPokemonByiD(pokemonId: number): Pokemon|undefined {
+    return POKEMONS.find(pokemon => pokemon.id == pokemonId)
+  }
+
+  getPokemonTypeList(): string[] {
+    return [
+      'Plante',
+      'Feu',
+      'Eau',
+      'Insecte',
+      'Normal',
+      'Electrik',
+      'Poison',
+      'Fée',
+      'Vol',
+      'Combat',
+      'Psy'
+    ];
+  }
+}
