ed588dc8618e1256e22e4306bcb9c708afe196eb
Teddy Bandama <teddybandama@MacBook-Pro-de-Teddy.local>
2022-12-13 11:02:00 +0100

    generation du guard auth ( canActivate)

---
+++ src/app/auth.guard.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  canActivate() {
+    console.log("le guard a bien été appelé !");
+    return true;
+  }
+}
