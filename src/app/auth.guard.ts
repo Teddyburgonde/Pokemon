1e01fefeb9624de815983fa84ae261ecc0b68f81
Teddy Bandama <teddybandama@MacBook-Pro-de-Teddy.local>
2022-12-13 11:58:13 +0100

    add canAtivate

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from './auth.service';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate() {
-    console.log("le guard a bien été appelé !");
-    return true;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ){}
+  canActivate():boolean {
+    if(this.authService.isLoggedIn){
+      return true;
+    }
+    this.router.navigate(['/login']);
+    return false;
   }
 }
